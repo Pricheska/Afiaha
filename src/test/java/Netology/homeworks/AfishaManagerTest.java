8dc1edb5aa3e4bc72fc73659e014e0e7e0a450c0
pricheska <den-ryab2019@yandex.ru>
2023-01-31 23:55:02 +0400

    Realisation after CI

--- src/test/java/Netology/homeworks/AfishaManagerTest.java
+++ src/test/java/Netology/homeworks/AfishaManagerTest.java
@@ -1,4 +1,37 @@
+package Netology.homeworks;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class AfishaManagerTest {
 
+    String film1 = "First";
+    String film2 = "Second";
+    String film3 = "Third";
+
+    @Test
+    public void test() {
+
+
+        AfishaManager manager = new AfishaManager();
+        manager.addFilms(film1);
+        manager.addFilms(film2);
+        manager.addFilms(film3);
+
+        String[] expected = {"First", "Second", "Third"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void ShouldShowMoviesInReverseOrder() {
+        AfishaManager manager = new AfishaManager();
+        manager.addFilms(film1);
+        manager.addFilms(film2);
+        manager.addFilms(film3);
+
+        String[] expected = {"Third", "Second", "First"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
